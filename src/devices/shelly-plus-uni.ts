b9b6cfe93f94a7d51623e90533f8c29a8e12407a
Lawrence Lee <llee123@gmail.com>
2024-10-05 00:10:47 -0700

    Add in Shelly plus uni identifier

---
+++ src/devices/shelly-plus-uni.ts
@@ -0,0 +1,45 @@
+import { component, Device } from './base';
+import {
+  BluetoothLowEnergy,
+  Cloud,
+  Input,
+  Mqtt,
+  OutboundWebSocket,
+  Script,
+  Switch,
+  WiFi,
+} from '../components';
+
+export class ShellyPlusUNI extends Device {
+  static readonly model: string = 'SNSN-0043X';
+  static readonly modelName: string = 'Shelly Plus UNI';
+
+  @component
+  readonly wifi = new WiFi(this);
+
+  @component
+  readonly bluetoothLowEnergy = new BluetoothLowEnergy(this);
+
+  @component
+  readonly cloud = new Cloud(this);
+
+  @component
+  readonly mqtt = new Mqtt(this);
+
+  @component
+  readonly outboundWebSocket = new OutboundWebSocket(this);
+
+  @component
+  readonly input0 = new Input(this, 0);
+
+  @component
+  readonly input1 = new Input(this, 1);
+
+  @component
+  readonly switch0 = new Switch(this, 0);
+
+  @component
+  readonly script = new Script(this);
+}
+
+Device.registerClass(ShellyPlusUNI);
